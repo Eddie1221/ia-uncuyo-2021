c3f5faddfe9703183536b31bf2afff5af555e9ce
Eduardo Pérez <88392382+EduardoPerez12@users.noreply.github.com>
2021-08-30 16:43:46 -0300

    Add files via upload

---
+++ tp3-busquedas-no-informadas/code/AgentBFS.py
@@ -0,0 +1,95 @@
+from Queue import*
+
+#Agente de busqueda a lo ancho
+class AgentBFS:
+    #Tiene como parametros la grilla, su posicion, el objetivo y la frontera
+    def __init__(self, Enviroment):
+        self.env = Enviroment
+        self.pos = Enviroment.agentPos
+        self.obj = Enviroment.objetivo
+        self.front = Queue()
+        self.front.enqueue(self.pos)
+        self.env.grilla[self.pos[0]][self.pos[1]] = "F"
+
+    #Funciones para moverse
+    def up(self, Fil, Col):
+        return self.env.accept_action(Fil, Col, "up")
+
+    def down(self, Fil, Col):
+        return self.env.accept_action(Fil, Col, "down")
+
+    def right(self, Fil, Col):
+        return self.env.accept_action(Fil, Col, "right")
+
+    def left(self, Fil, Col):
+        return self.env.accept_action(Fil, Col, "left")
+
+    #Funcion para hacer la busqueda que imprime la grilla y la secuencia, es una funcion wrapper para la funcion recursiva de busqueda a lo ancho
+    def think(self):
+        Secuencia = BFSsearch(self)
+        self.env.grilla[self.pos[0]][self.pos[1]] = "A"
+        self.env.grilla[self.obj[0]][self.obj[1]] = "X"
+        print("A = Agente")
+        print("X = Objetivo")
+        print("O = Obstaculo")
+        print("N = No explorado")
+        print("F = Frontera")
+        print("E = Explorado")
+        self.env.print_enviroment()
+        print("Estado inicial del Agente: ", self.pos)
+        print("Estado Objetivo: ", self.obj)
+        print(Secuencia)
+        if type(Secuencia) != str:
+            print("El largo de la secuencia es: ",len(Secuencia))
+            return len(Secuencia)
+
+#Funcion auxiliar para calcula la resta de 2 estados
+def Restar_Estados(Est1, Est2):
+    Res = []
+    Res.append(Est1[0]-Est2[0])
+    Res.append(Est1[1]-Est2[1])
+    return Res
+
+#Funcion auxiliar para saber si 2 estado son adyacentes, esta se utiliza al salir de la recursividad y obtener los estados pertenecienes 
+# a la secuencia deseada
+def is_next(Est1, Est2):
+    Resta = Restar_Estados(Est1, Est2)
+    if Resta == [0,1] or Resta == [0,-1] or Resta == [1,0] or Resta == [-1,0]:
+        return True
+
+#Funcion recursiva de busqueda a lo ancho implementando cola FIFO
+def BFSsearch(self):
+
+        while self.front.head != None:
+            Estado = self.front.dequeue()
+            self.env.grilla[Estado[0]][Estado[1]] = "E"
+
+            if Estado == self.obj:
+                Lista = []
+                Lista.append(Estado)
+                return Lista
+            else:
+
+                if self.up(Estado[0], Estado[1]) == True:
+                    self.env.grilla[Estado[0]-1][Estado[1]] = "F"
+                    self.front.enqueue([Estado[0]-1,Estado[1]])
+
+                if self.down(Estado[0], Estado[1]) == True:
+                    self.env.grilla[Estado[0]+1][Estado[1]] = "F"
+                    self.front.enqueue([Estado[0]+1,Estado[1]])
+
+                if self.right(Estado[0], Estado[1]) == True:
+                    self.env.grilla[Estado[0]][Estado[1]+1] = "F"
+                    self.front.enqueue([Estado[0],Estado[1]+1])
+
+                if self.left(Estado[0], Estado[1]) == True:
+                    self.env.grilla[Estado[0]][Estado[1]-1] = "F"
+                    self.front.enqueue([Estado[0],Estado[1]-1])
+
+                Lista = BFSsearch(self)
+                if type(Lista) != str:
+                    if is_next(Lista[0],Estado) == True:
+                        Lista.insert(0,Estado)
+                    return Lista
+
+        return "No fue posible llegar al objetivo."
